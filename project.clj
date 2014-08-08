320c8e101b562b980dd313305e311a202bb47e8b
jules <jules_gosnell@yahoo.com>
2014-08-08 20:07:34 +0100

    upgrade clojure

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]]
 
   :plugins [[lein-test-out "0.3.1"]
             [lein-nodisassemble "0.1.3"]]
