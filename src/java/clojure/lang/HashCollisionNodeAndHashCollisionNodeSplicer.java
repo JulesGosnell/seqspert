46b2dfbd05733fecf4120fc019a6632dda925f87
Jules Gosnell <jules_gosnell@yahoo.com>
2014-09-17 19:09:58 +0100

    test and implement splicing of HCNs

--- src/java/clojure/lang/HashCollisionNodeAndHashCollisionNodeSplicer.java
+++ src/java/clojure/lang/HashCollisionNodeAndHashCollisionNodeSplicer.java
@@ -1,32 +1,76 @@
 package clojure.lang;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+import clojure.lang.PersistentHashMap;
+import clojure.lang.PersistentHashMap.BitmapIndexedNode;
 import clojure.lang.PersistentHashMap.HashCollisionNode;
 import clojure.lang.PersistentHashMap.INode;
 
 class HashCollisionNodeAndHashCollisionNodeSplicer extends AbstractSplicer {
-	public INode splice(int shift, Duplications duplications, Object leftKey, Object leftValue, int rightHash, Object rightKey, Object rightValue) {
-		final HashCollisionNode leftNode  = (HashCollisionNode) leftValue;
-		final HashCollisionNode rightNode = (HashCollisionNode) rightValue;
 
-		// TODO: is it possible that the nodes hash codes are different ?
-		// I think they must fall in the same partition, but can be different - consider...
-		// try to replicate from clojure...
+    // TODO - think of a good name...
+    public INode foo(int shift,
+		     int leftHash, HashCollisionNode leftNode,
+		     int rightHash, HashCollisionNode rightNode) {
+	final AtomicReference<Thread> edit = null;
+	
+	final int leftBits = PersistentHashMap.mask(leftHash, shift);
+	final int rightBits = PersistentHashMap.mask(rightHash, shift);
+	if (leftBits == rightBits) {
+	    // keep recursing down...
+	    return new BitmapIndexedNode(edit,
+					 1 << leftBits,
+					 new Object[]{null,
+						      foo(shift + 5,
+							  leftHash, leftNode,
+							  rightHash, rightNode)});
+	} else {
+	    // end recursion
+	    // c.f. NodeUtils.create() - we should share code somehow
+	    return new BitmapIndexedNode(edit,
+					 1 << leftBits | 1 << rightBits,
+					 (leftBits <= rightBits) ?
+					 new Object[]{null, leftNode, null, rightNode} :
+					 new Object[]{null, rightNode, null, leftNode});
+	}
+    }
+    
+    
+    public INode splice(int shift, Duplications duplications, Object leftKey, Object leftValue, int rightHash, Object rightKey, Object rightValue) {
+	final HashCollisionNode leftNode  = (HashCollisionNode) leftValue;
+	final HashCollisionNode rightNode = (HashCollisionNode) rightValue;
+
+	// TODO: is it possible that the nodes hash codes are different ?
+	// I think they must fall in the same partition, but can be different - consider...
+	// try to replicate from clojure...
+
+	if (leftNode.hash == rightNode.hash) {
 		
-		final int leftLength = leftNode.count * 2;
-		final int rightLength = rightNode.count* 2;
-		final Object[] leftArray = leftNode.array;
-		final int oldDuplications = duplications.duplications;
+	    final int leftLength = leftNode.count * 2;
+	    final int rightLength = rightNode.count* 2;
+	    final Object[] leftArray = leftNode.array;
+	    final int oldDuplications = duplications.duplications;
 
-		final Object[] newArray = HashCollisionNodeUtils.maybeAddAll(leftArray, leftLength,
-				rightNode.array,
-				rightLength,
-				duplications);
+	    final Object[] newArray = HashCollisionNodeUtils.maybeAddAll(leftArray, leftLength,
+									 rightNode.array,
+									 rightLength,
+									 duplications);
 
-		return newArray == leftArray ?
-				leftNode :
-					new HashCollisionNode(null,
-							leftNode.hash,
-							(leftLength + rightLength - ((duplications.duplications - oldDuplications) * 2)) / 2,
-							newArray);
-	}	
+	    return newArray == leftArray ?
+		leftNode :
+		new HashCollisionNode(null,
+				      leftNode.hash,
+				      (leftLength + rightLength - ((duplications.duplications - oldDuplications) * 2)) / 2,
+				      newArray);
+	} else {
+	    
+	    // recursively build BINS and shift by 5 until hashes fall
+	    // into different partitions, then build a BIN with two
+	    // HCN elements...
+
+	    return foo(shift, leftNode.hash, leftNode, rightNode.hash, rightNode);
+	}
+    }
+	
 }
--- test/java/clojure/lang/HashCollisionNodeAndHashCollisionNodeSplicerTest.java
+++ test/java/clojure/lang/HashCollisionNodeAndHashCollisionNodeSplicerTest.java
@@ -9,116 +9,119 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.junit.Test;
 
 import clojure.lang.PersistentHashMap.HashCollisionNode;
+import clojure.lang.PersistentHashMap.BitmapIndexedNode;
 import clojure.lang.PersistentHashMap.INode;
 
 public class HashCollisionNodeAndHashCollisionNodeSplicerTest implements SplicerTestInterface {
 
-	@Test
-	@Override
-	public void testNoCollision() {
-		final int shift = 0;
-		final int leftHashCode = 1;
-		final int rightHashCode = 2;
-		final Object key0 = new HashCodeKey("key0", leftHashCode);
-		final Object key1 = new HashCodeKey("key1", leftHashCode);
-		final Object key2 = new HashCodeKey("key2", rightHashCode);
-		final Object key3 = new HashCodeKey("key3", rightHashCode);
-		final Object value0 = "value0";
-		final Object value1 = "value1";
-		final Object value2 = "value2";
-		final Object value3 = "value3";
-		final HashCollisionNode leftNode   = new HashCollisionNode(null, leftHashCode,  2, new Object[]{key0, value0, key1, value1});
-		final HashCollisionNode rightNode =  new HashCollisionNode(null, rightHashCode, 2, new Object[]{key2, value2, key3, value3});
-
-		final AtomicReference<Thread> edit = new AtomicReference<Thread>();
-		INode expected = leftNode;
-		//    	INode expected = BitmapIndexedNode.EMPTY;
-		Box addedLeaf = null;
-		int expectedDuplications = 0;
-		//    	addedLeaf = new Box(null);
-		//    	expected = expected.assoc(edit, shift, hashCode, key0, value0, addedLeaf);
-		//    	expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
-		//    	addedLeaf = new Box(null);	
-		//    	expected = expected.assoc(edit, shift, hashCode, key1, value1, addedLeaf);
-		//    	expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
-		addedLeaf = new Box(null);
-		expected = expected.assoc(edit, shift, hashCode, key2, value2, addedLeaf);
-		expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
-		addedLeaf = new Box(null);	
-		expected = expected.assoc(edit, shift, hashCode, key3, value3, addedLeaf);
-		expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
-
-		final Duplications duplications = new Duplications(0);
-		final HashCollisionNode actual =  (HashCollisionNode) NodeUtils.splice(shift, duplications, null, leftNode, 0, null, rightNode);
-		assertEquals(expectedDuplications, duplications.duplications);
-	}
-
+    @Test
+    @Override
+    public void testNoCollision() {
 	final int shift = 0;
-	final int hashCode = 2;
-	final Object key0 = new HashCodeKey("key0", hashCode);
-	final Object key1 = new HashCodeKey("key1", hashCode);
-	final Object key2 = new HashCodeKey("key2", hashCode);
-	final Object key3 = new HashCodeKey("key3", hashCode);
+	final int leftHashCode = 1;
+	final int rightHashCode = 33;
+	final Object key0 = new HashCodeKey("key0", leftHashCode);
+	final Object key1 = new HashCodeKey("key1", leftHashCode);
+	final Object key2 = new HashCodeKey("key2", rightHashCode);
+	final Object key3 = new HashCodeKey("key3", rightHashCode);
 	final Object value0 = "value0";
 	final Object value1 = "value1";
 	final Object value2 = "value2";
 	final Object value3 = "value3";
+	final HashCollisionNode leftNode   = new HashCollisionNode(null, leftHashCode,  2, new Object[]{key0, value0, key1, value1});
+	final HashCollisionNode rightNode =  new HashCollisionNode(null, rightHashCode, 2, new Object[]{key2, value2, key3, value3});
 
-	public void test(Object key0, Object value0, Object key1, Object value1,
-			Object key2, Object value2, Object key3, Object value3,
-			boolean same) {
-		final HashCollisionNode leftNode   = new HashCollisionNode(null, hashCode, 2, new Object[]{key0, value0, key1, value1});
-		final HashCollisionNode rightNode =  new HashCollisionNode(null, hashCode, 2, new Object[]{key2, value2, key3, value3});
+	final AtomicReference<Thread> edit = new AtomicReference<Thread>();
+	INode expected = BitmapIndexedNode.EMPTY;
+	Box addedLeaf = null;
+	int expectedDuplications = 0;
+	addedLeaf = new Box(null);
+	expected = expected.assoc(edit, shift, key0.hashCode(), key0, value0, addedLeaf);
+	expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
+	addedLeaf = new Box(null);	
+	expected = expected.assoc(edit, shift, key1.hashCode(), key1, value1, addedLeaf);
+	expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
+	addedLeaf = new Box(null);
+	expected = expected.assoc(edit, shift, key2.hashCode(), key2, value2, addedLeaf);
+	expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
+	addedLeaf = new Box(null);	
+	expected = expected.assoc(edit, shift, key3.hashCode(), key3, value3, addedLeaf);
+	expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
 
-		final AtomicReference<Thread> edit = new AtomicReference<Thread>();
-		HashCollisionNode expected = (HashCollisionNode) leftNode;
-		Box addedLeaf = null;
-		int expectedDuplications = 0;
-		addedLeaf = new Box(null);
-		expected = (HashCollisionNode) expected.assoc(edit, shift, hashCode, key2, value2, addedLeaf);
-		expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
-		addedLeaf = new Box(null);	
-		expected = (HashCollisionNode) expected.assoc(edit, shift, hashCode, key3, value3, addedLeaf);
-		expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
+	final Duplications duplications = new Duplications(0);
+	final INode actual = NodeUtils.splice(shift, duplications, null, leftNode, rightHashCode, null, rightNode);
+	// TODO: fix this line
+	//assertEquals(expected, actual);
 
-		final Duplications duplications = new Duplications(0);
-		final HashCollisionNode actual =  (HashCollisionNode) NodeUtils.splice(shift, duplications, null, leftNode, 0, null, rightNode);
-		assertEquals(expectedDuplications, duplications.duplications);
-		assertHashCollisionNodesEqual(expected, actual);
-		if (same) assertSame(expected, actual);
-	}
+	assertEquals(expectedDuplications, duplications.duplications);
+    }
 
-	@Test
-	@Override
-	public void testCollision() {
-		// differing keys all have same hashcode but values are different...
-		test(key0, value0, key1, value1, key2, value2, key3, value3, false);
-		test(key0, value0, key1, value1, key3, value3, key2, value2, false);
-	}
+    final int shift = 0;
+    final int hashCode = 2;
+    final Object key0 = new HashCodeKey("key0", hashCode);
+    final Object key1 = new HashCodeKey("key1", hashCode);
+    final Object key2 = new HashCodeKey("key2", hashCode);
+    final Object key3 = new HashCodeKey("key3", hashCode);
+    final Object value0 = "value0";
+    final Object value1 = "value1";
+    final Object value2 = "value2";
+    final Object value3 = "value3";
 
-	@Test
-	@Override
-	public void testDuplication() {
-		// as above, but one pair of keys is identical...
-		final Object leftValue1 = "left-" + (String) value1;
-		final Object rightValue1 = "right-" + (String) value1;
-		test(key0, value0, key1, leftValue1, key1, rightValue1, key2, value2, false);
-		test(key0, value0, key1, leftValue1, key2, value2, key1, rightValue1, false);
-	}
+    public void test(Object key0, Object value0, Object key1, Object value1,
+		     Object key2, Object value2, Object key3, Object value3,
+		     boolean same) {
+	final HashCollisionNode leftNode   = new HashCollisionNode(null, hashCode, 2, new Object[]{key0, value0, key1, value1});
+	final HashCollisionNode rightNode =  new HashCollisionNode(null, hashCode, 2, new Object[]{key2, value2, key3, value3});
 
-	@Test
-	//@Override
-	public void testSomeIdentical() {
-		// as above but one pair of values is also identical...
-		test(key0, value0, key1, value1, key1, value1, key2, value2, false);
-		test(key0, value0, key1, value1, key2, value2, key1, value1, false);
-	}
+	final AtomicReference<Thread> edit = new AtomicReference<Thread>();
+	HashCollisionNode expected = (HashCollisionNode) leftNode;
+	Box addedLeaf = null;
+	int expectedDuplications = 0;
+	addedLeaf = new Box(null);
+	expected = (HashCollisionNode) expected.assoc(edit, shift, hashCode, key2, value2, addedLeaf);
+	expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
+	addedLeaf = new Box(null);	
+	expected = (HashCollisionNode) expected.assoc(edit, shift, hashCode, key3, value3, addedLeaf);
+	expectedDuplications += (addedLeaf.val == addedLeaf) ? 0 : 1;
 
-	@Test
-	//@Override
-	public void testAllIdentical() {
-		// all keys and values are also identical...
-		test(key0, value0, key1, value1, key0, value0, key1, value1, true);
-	}
+	final Duplications duplications = new Duplications(0);
+	final HashCollisionNode actual =  (HashCollisionNode) NodeUtils.splice(shift, duplications, null, leftNode, 0, null, rightNode);
+	assertEquals(expectedDuplications, duplications.duplications);
+	assertHashCollisionNodesEqual(expected, actual);
+	if (same) assertSame(expected, actual);
+    }
+
+    @Test
+    @Override
+    public void testCollision() {
+	// differing keys all have same hashcode but values are different...
+	test(key0, value0, key1, value1, key2, value2, key3, value3, false);
+	test(key0, value0, key1, value1, key3, value3, key2, value2, false);
+    }
+
+    @Test
+    @Override
+    public void testDuplication() {
+	// as above, but one pair of keys is identical...
+	final Object leftValue1 = "left-" + (String) value1;
+	final Object rightValue1 = "right-" + (String) value1;
+	test(key0, value0, key1, leftValue1, key1, rightValue1, key2, value2, false);
+	test(key0, value0, key1, leftValue1, key2, value2, key1, rightValue1, false);
+    }
+
+    @Test
+    //@Override
+    public void testSomeIdentical() {
+	// as above but one pair of values is also identical...
+	test(key0, value0, key1, value1, key1, value1, key2, value2, false);
+	test(key0, value0, key1, value1, key2, value2, key1, value1, false);
+    }
+
+    @Test
+    //@Override
+    public void testAllIdentical() {
+	// all keys and values are also identical...
+	test(key0, value0, key1, value1, key0, value0, key1, value1, true);
+    }
 
 }
