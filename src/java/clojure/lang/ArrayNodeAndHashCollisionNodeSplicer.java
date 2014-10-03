5116e944338c611aadc4b006ecb506ba842057de
Jules Gosnell <jules_gosnell@yahoo.com>
2014-10-03 19:13:41 +0100

    new splicer and test that need fixing

--- src/java/clojure/lang/ArrayNodeAndHashCollisionNodeSplicer.java
+++ src/java/clojure/lang/ArrayNodeAndHashCollisionNodeSplicer.java
@@ -7,19 +7,24 @@ import clojure.lang.PersistentHashMap.INode;
 
 class ArrayNodeAndHashCollisionNodeSplicer extends AbstractSplicer {
 
-    // public INode splice(int shift, Counts counts,
-    // 			Object leftKey, Object leftValue,
-    // 			int rightHash, Object rightKey, Object rightValue) {
-    // 	final HashCollisionNode leftNode  = (HashCollisionNode) leftValue;
-    // 	final ArrayNode rightNode = (ArrayNode) rightValue;
+    public INode splice(int shift, Counts counts,
+    			Object leftKey, Object leftValue,
+			int rightHash, Object rightKey, Object rightValue) {
+    	final ArrayNode leftNode = (ArrayNode) leftValue;
+    	final HashCollisionNode rightNode  = (HashCollisionNode) rightValue;
 	
-    // 	final INode[] rightArray = rightNode.array;
-    // 	final int index = PersistentHashMap.mask(rightHash, shift);
-    // 	final INode subNode = rightArray[index];
-    // 	return (subNode == null) ?
-    // 	    new ArrayNode(null, rightNode.count + 1, cloneAndSet(rightArray, index + 1, leftNode)) :
-    // 	    NodeUtils.splice(shift, counts, null, leftNode, 0, null, subNode);
-    // 	// TODO: we are passing 0 as rightHash - need to make sure that it is never used...
-    // }
+    	final INode[] leftArray = leftNode.array;
+    	final int index = PersistentHashMap.mask(rightHash, shift);
+    	final INode subNode = leftArray[index];
+    	if (subNode == null) {
+    	    return new ArrayNode(null, leftNode.count + 1, cloneAndSet(leftArray, index, rightNode));
+	} else {
+    	    final INode newNode = NodeUtils.splice(shift, counts, null, subNode, rightHash, null, rightNode);
+	    if (subNode == newNode)
+		return leftNode;
+	    else
+		return new ArrayNode(null, leftNode.count + 1, cloneAndSet(leftArray, index, newNode));
+	}
+    }
 
 }
