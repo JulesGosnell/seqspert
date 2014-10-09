9079ed0d8b8f0b169154741b24b50287debae699
Jules Gosnell <jules_gosnell@yahoo.com>
2014-10-09 22:56:10 +0100

    more comments

--- src/java/clojure/lang/KeyValuePairAndBitmapIndexedNodeSplicer.java
+++ src/java/clojure/lang/KeyValuePairAndBitmapIndexedNodeSplicer.java
@@ -5,6 +5,7 @@ import clojure.lang.PersistentHashMap.BitmapIndexedNode;
 import clojure.lang.PersistentHashMap.HashCollisionNode;
 import clojure.lang.PersistentHashMap.INode;
 
+// why is this so complicated ?
 class KeyValuePairAndBitmapIndexedNodeSplicer implements Splicer {
 
     public INode splice(int shift, Counts counts,
@@ -12,23 +13,30 @@ class KeyValuePairAndBitmapIndexedNodeSplicer implements Splicer {
                         int _, Object rightKey, Object rightValue) {
 
         final BitmapIndexedNode rightNode = (BitmapIndexedNode) rightValue;
+        final Object[] rightArray = rightNode.array;
+        final int rightBitmap = rightNode.bitmap;
         final int leftHash = hash(leftKey);
         final int bit = BitmapIndexedNodeUtils.bitpos(leftHash, shift);
-        if((rightNode.bitmap & bit) == 0) {
+        final int index = rightNode.index(bit) * 2;
+        if((rightBitmap & bit) == 0) {
             // rhs unoccupied
-            // TODO - do not use assoc here - reference similar code
-            // TODO: need a BIN insert fn.... how is that different from assoc ?
+
             // TODO: consider whether to return a BIN or an AN
-            // TODO: inline logic and lose Box churn ...
-            // TODO: this must be wrong - we are overlaying left over right ?
-            final Box addedLeaf = new Box(null);
-            final INode n = rightNode.assoc(shift, leftHash, leftKey, leftValue, addedLeaf);
-            counts.sameKey += (addedLeaf.val == null ? 1 : 0);
-            return n;
+            final int rightBitCount = Integer.bitCount(rightBitmap);
+
+            // lets assume that we could not have received an empty
+            // BIN, therefore we have at least 2 subNodes, so there is
+            // no need to consider returning a null from this
+            // branch...
+            return new BitmapIndexedNode(null,
+                                         rightBitmap | bit,
+                                         NodeUtils.cloneAndInsert(rightArray,
+                                                                  rightBitCount * 2,
+                                                                  index,
+                                                                  leftKey,
+                                                                  leftValue));
         } else {
             // rhs occupied...
-            final int index = rightNode.index(bit) * 2;
-            final Object[] rightArray = rightNode.array;
             final Object subKey = rightArray[index];
             final Object subValue = rightArray[index + 1];
             final int oldSameKey = counts.sameKey;
@@ -36,10 +44,9 @@ class KeyValuePairAndBitmapIndexedNodeSplicer implements Splicer {
             final INode spliced = NodeUtils.splice(shift + 5, counts,
                                                    leftKey, leftValue,
                                                    NodeUtils.nodeHash(subKey), subKey, subValue);
-            if ((~bit & rightNode.bitmap) > 0) {
+            if ((~bit & rightBitmap) > 0) {
                 // the BIN contains more than just this entry
                 if (spliced == null) {
-                    System.out.println("AAARGH! - splice() returned null - other entries");
                     // the LHS key and maybe value are the same as those in the RHS
                     // if the value were different it would replace the one on the LHS
                     // if the value were the same, then we do not need to change the one in the RHS
@@ -49,10 +56,11 @@ class KeyValuePairAndBitmapIndexedNodeSplicer implements Splicer {
                     System.out.println("AAARGH! - splice() didn't return null - other entries");
                     // we have successfully merged the LHS and RHS entry
                     // we need to copy over the rest of the RHS and return a new BIN...
+                    // since we are replacing a subNode we do not have to worry about promotion to an AN.
                     return new BitmapIndexedNode(null,
-                                                 rightNode.bitmap | bit,
+                                                 rightBitmap,
                                                  NodeUtils.cloneAndInsert(rightArray,
-                                                                          Integer.bitCount(rightNode.bitmap) * 2,
+                                                                          Integer.bitCount(rightBitmap) * 2,
                                                                           index,
                                                                           null,
                                                                           spliced));
@@ -60,17 +68,15 @@ class KeyValuePairAndBitmapIndexedNodeSplicer implements Splicer {
             } else {
                 // the BIN only contains this entry
                 if (spliced == null) {
-                    System.out.println("AAARGH! - splice() returned null - only entry");
                     // we must only return a single KVP which we cannot do, so we return null
                                                                                 
+                    System.out.println("WARN: I think we are returning wrong value...");
                     // return spliced;
                                                                 
                     // TODO: I think that we should return spliced
                     // here - but I have lots of tests expecting rightNode
-
                     return rightNode;
                 } else {
-                    System.out.println("AAARGH! - splice() didn't return null - only entry");
                     // we only need to return this single spliced node
                     return spliced;
                 }
