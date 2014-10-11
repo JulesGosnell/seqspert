a0840b8ac7fcce1669b44ffefb8684d4371eb897
Jules Gosnell <jules_gosnell@yahoo.com>
2014-10-11 15:32:47 +0100

    lose dead code and tidy up

--- src/java/clojure/lang/ArrayNodeAndHashCollisionNodeSplicer.java
+++ src/java/clojure/lang/ArrayNodeAndHashCollisionNodeSplicer.java
@@ -17,25 +17,17 @@ class ArrayNodeAndHashCollisionNodeSplicer implements Splicer {
         final int rightHash = rightNode.hash;
         final int index = PersistentHashMap.mask(rightHash, shift);
         final INode subNode = leftArray[index];
-        if (subNode == null) {
-            final INode tmp = BitmapIndexedNodeUtils.create(
-                                                            0, // TODO - this 0 wrong ?
-                                                            //BitmapIndexedNodeUtils.bitpos(rightHash, shift + 5),
-                                                            //BitmapIndexedNodeUtils.bitpos(rightHash, shift),
-                                                            rightNode);
 
-            return new ArrayNode(null, leftNode.count + 1, NodeUtils.cloneAndSetNode(leftArray, index, tmp));
+        int newCount;
+        INode newNode;
+        if (subNode == null) {
+            newCount = leftNode.count + 1;
+            newNode = BitmapIndexedNodeUtils.create(0, rightNode); // TODO - this 0 wrong ?
         } else {
-            final INode newNode = NodeUtils.splice(
-                                                   //shift,
-                                                   shift + 5,
-                                                   counts,
-                                                   null, subNode,
-                                                   rightHash, null, rightNode);
-            return (subNode == newNode) ?
-                leftNode:
-                new ArrayNode(null, leftNode.count, NodeUtils.cloneAndSetNode(leftArray, index, newNode));
+            newCount = leftNode.count;;
+            newNode = NodeUtils.splice(shift + 5, counts, null, subNode, rightHash, null, rightNode);
         }
+        return new ArrayNode(null, newCount, NodeUtils.cloneAndSetNode(leftArray, index, newNode));
     }
 
 }
