a3d9a0eb4116a2eafcf82ba45ce289244035bb53
Jules Gosnell <jules_gosnell@yahoo.com>
2014-10-12 23:15:48 +0100

    new tests for BIN -> AN promotion

--- test/java/clojure/lang/BitmapIndexedNodeAndHashCollisionNodeSplicerTest.java
+++ test/java/clojure/lang/BitmapIndexedNodeAndHashCollisionNodeSplicerTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertSame;
 import org.junit.Test;
 
 import clojure.lang.PersistentHashMap.INode;
+import clojure.lang.PersistentHashMap.BitmapIndexedNode;
 
 public class BitmapIndexedNodeAndHashCollisionNodeSplicerTest implements SplicerTestInterface {
 
@@ -37,6 +38,31 @@ public class BitmapIndexedNodeAndHashCollisionNodeSplicerTest implements Splicer
         if (same) assertSame(expectedNode, actualNode);
     }
 
+    public void test(int leftStart, int leftEnd,
+                     int rightHash,
+                     Object rightKey0, Object rightValue0, Object rightKey1, Object rightValue1,
+                     boolean same) {
+
+        final INode empty = BitmapIndexedNode.EMPTY;
+        final INode leftNode = TestUtils.assocN(shift, empty, leftStart, leftEnd, new Counts());
+        final INode rightNode = HashCollisionNodeUtils.create(rightHash,
+                                                              rightKey0, rightValue0,
+                                                              rightKey1, rightValue1);
+        final Counts expectedCounts = new Counts();
+        final INode expectedNode =
+            TestUtils.assoc(shift,
+                            TestUtils.assoc(shift, leftNode, rightKey0, rightValue0, expectedCounts),
+                            rightKey1, rightValue1,
+                            expectedCounts);
+
+        final Counts actualCounts = new Counts();
+        final INode actualNode = splicer.splice(shift, actualCounts, null, leftNode, null, rightNode);
+
+        assertEquals(expectedCounts, actualCounts);
+        assertNodeEquals(expectedNode, actualNode);
+        if (same) assertSame(expectedNode, actualNode);
+    }
+
     @Override
     @Test
     public void testDifferent() {
@@ -45,12 +71,24 @@ public class BitmapIndexedNodeAndHashCollisionNodeSplicerTest implements Splicer
              new HashCodeKey("key1.0", 1), "value1.0",
              new HashCodeKey("key1.1", 1), "value1.1",
              false);
+        // test(3, 18,
+        //      1,
+        //      new HashCodeKey("key1.0", 1), "value1.0",
+        //      new HashCodeKey("key1.1", 1), "value1.1",
+        //      false)
+            ;
+
     }
 
     @Override
     @Test
     public void testSameKeyHashCode() {
         test(new HashCodeKey("key1.0", 1), "value1.0",
+             1,
+             new HashCodeKey("key1.1", 1), "value1.1",
+             new HashCodeKey("key1.2", 1), "value1.2",
+             false);
+        test(1, 16,
              1,
              new HashCodeKey("key1.1", 1), "value1.1",
              new HashCodeKey("key1.2", 1), "value1.2",
@@ -65,6 +103,11 @@ public class BitmapIndexedNodeAndHashCollisionNodeSplicerTest implements Splicer
              new HashCodeKey("key1.0", 1), "value1.0.1",
              new HashCodeKey("key1.1", 1), "value1.1",
              false);
+        test(1, 16,
+             1,
+             new HashCodeKey("key1",   1), "value1.0",
+             new HashCodeKey("key1.1", 1), "value1.1",
+             false);
     }
 
 
@@ -74,6 +117,11 @@ public class BitmapIndexedNodeAndHashCollisionNodeSplicerTest implements Splicer
         test(new HashCodeKey("key1.0", 1), "value1.0",
              1,
              new HashCodeKey("key1.0", 1), "value1.0",
+             new HashCodeKey("key1.1", 1), "value1.1",
+             false);
+        test(1, 16,
+             1,
+             new HashCodeKey("key1",   1), "value1",
              new HashCodeKey("key1.1", 1), "value1.1",
              false);
     }
