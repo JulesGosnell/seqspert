679cc625794698265ad991599afd5aab2e065a8a
jules <jules_gosnell@yahoo.com>
2014-10-12 17:47:38 +0100

    begin to support the promotion of BINs to ANs

--- src/java/clojure/lang/BitmapIndexedNodeAndBitmapIndexedNodeSplicer.java
+++ src/java/clojure/lang/BitmapIndexedNodeAndBitmapIndexedNodeSplicer.java
@@ -7,80 +7,86 @@ import clojure.lang.PersistentHashMap.INode;
 
 class BitmapIndexedNodeAndBitmapIndexedNodeSplicer implements Splicer {
 
-    public INode splice(int shift, Counts counts,
-                        Object leftKey, Object leftValue,
-                        Object rightKey, Object rightValue) {
+	public INode splice(int shift, Counts counts,
+			Object leftKey, Object leftValue,
+			Object rightKey, Object rightValue) {
 
-        final BitmapIndexedNode leftNode = (BitmapIndexedNode) leftValue;
-        final BitmapIndexedNode rightNode = (BitmapIndexedNode) rightValue;
+		final BitmapIndexedNode leftNode = (BitmapIndexedNode) leftValue;
+		final BitmapIndexedNode rightNode = (BitmapIndexedNode) rightValue;
 
-        final int leftBitmap = leftNode.bitmap;
-        final Object[] leftArray = leftNode.array;
-        final int rightBitmap = rightNode.bitmap;
-        final Object[] rightArray = rightNode.array;
+		final int leftBitmap = leftNode.bitmap;
+		final Object[] leftArray = leftNode.array;
+		final int rightBitmap = rightNode.bitmap;
+		final Object[] rightArray = rightNode.array;
 
-        final int newBitmap = leftBitmap | rightBitmap;
-        final int newBitCount = Integer.bitCount(newBitmap); // nasty - but we need to know...
-        final Object[] newArray = new Object[newBitCount * 2];
+		final int newBitmap = leftBitmap | rightBitmap;
+		final int newBitCount = Integer.bitCount(newBitmap); // nasty - but we need to know...
+		final Object[] newBinArray = new Object[newBitCount * 2];
+		final INode[] newAnArray = new INode[32];
 
-        // if (newBitCount > 16) {
-        //     // output will be an ArrayNode...
-        //     // TODO: this is breaking Clojure tests...
-        //     throw new RuntimeException("NYI");
-        // } else 
-        {
-            // output will be a BitmapIndexedNode or a HashCollisionNode
-            int lPosition = 0;
-            int rPosition = 0;
-            int oPosition = 0;
-            int differences = 0;
-            for (int i = 0; i < 32; i++) {
-                int mask = 1 << i;
-                boolean lb = ((leftBitmap & mask) != 0);
-                boolean rb = ((rightBitmap & mask) != 0);
+		final boolean promoted = (newBitCount > 16); // TODO: handle this more efficiently
+		// output will be a BitmapIndexedNode or a HashCollisionNode
+		int leftIndex = 0;
+		int rightIndex = 0;
+		int newBinIndex = 0;
+		int differences = 0;
+		for (int i = 0; i < 32; i++) {
+			int mask = 1 << i;
+			boolean lb = ((leftBitmap & mask) != 0);
+			boolean rb = ((rightBitmap & mask) != 0);
 
-                // maybe we should make this check in splice() ?
-                if (lb) {
-                    if (rb) {
-                        Object lk = leftArray[lPosition++];
-                        Object lv = leftArray[lPosition++];
-                        Object rk = rightArray[rPosition++];
-                        Object rv = rightArray[rPosition++];
+			// maybe we should make this check in splice() ?
+			if (lb) {
+				if (rb) {
+					Object lk = leftArray[leftIndex++];
+					Object lv = leftArray[leftIndex++];
+					Object rk = rightArray[rightIndex++];
+					Object rv = rightArray[rightIndex++];
 
-                        // TODO: ouch
-                        final INode newNode = NodeUtils.splice(shift + 5, counts, lk, lv, rk, rv);
-                        if (newNode == null) {
-                            // we must have spliced two leaves giving a result of a single leaf...
-                            // the key must be unchanged
-                            newArray[oPosition++] = lk;
-                            // what is the value ? TODO: ouch - expensive and duplicate computation
-                            final boolean same = Util.equiv(lv, rv);
-                            newArray[oPosition++] = same ? lv : rv;
-                            differences += same ? 0 : 1;
-                        } else {
-                            // result was a Node...
-                            newArray[oPosition++] = null;
-                            newArray[oPosition++] = newNode;
-                            final boolean same = lv == newNode;
-                            differences += same ? 0 : 1;
-                        }
-                    } else {
-                        newArray[oPosition++] = leftArray[lPosition++];
-                        newArray[oPosition++] = leftArray[lPosition++];
-                    }
-                } else {
-                    if (rb) {
-                        newArray[oPosition++] = rightArray[rPosition++];
-                        newArray[oPosition++] = rightArray[rPosition++];
-                        differences++;
-                    } 
-                }
-            }
+					// TODO: ouch
+					final INode newNode = NodeUtils.splice(shift + 5, counts, lk, lv, rk, rv);
+					if (newNode == null) {
+						// we must have spliced two leaves giving a result of a single leaf...
+						// the key must be unchanged
+						newBinArray[newBinIndex++] = lk;
+						// what is the value ? TODO: ouch - expensive and duplicate computation
+						final boolean same = Util.equiv(lv, rv);
+						final Object newValue = same ? lv : rv;
+						newBinArray[newBinIndex++] = newValue;
+						newAnArray[i] = NodeUtils.create(shift + 5, lk, newValue);
+						differences += same ? 0 : 1;
+					} else {
+						// result was a Node...
+						newBinArray[newBinIndex++] = null;
+						newBinArray[newBinIndex++] = newNode;
+						newAnArray[i] = newNode;
+						final boolean same = lv == newNode;
+						differences += same ? 0 : 1;
+					}
+				} else {
+					final Object newKey = leftArray[leftIndex++];
+					final Object newValue = leftArray[leftIndex++];
+					newBinArray[newBinIndex++] = newKey;
+					newBinArray[newBinIndex++] = newValue;
+					newAnArray[i] = (newKey == null) ? (INode) newValue : NodeUtils.create(shift + 5, newKey, newValue);
+				}
+			} else {
+				if (rb) {
+					final Object newKey = rightArray[rightIndex++];
+					final Object newValue = rightArray[rightIndex++];
+					newBinArray[newBinIndex++] = newKey;
+					newBinArray[newBinIndex++] = newValue;
+					newAnArray[i] = (newKey == null) ? (INode) newValue : NodeUtils.create(shift + 5, newKey, newValue);
+					differences++;
+				} 
+			}
+		}
 
-            return differences == 0 ?
-                leftNode :
-                new PersistentHashMap.BitmapIndexedNode(new AtomicReference<Thread>(), newBitmap, newArray);
-        }
-    }
+		return differences == 0 ?
+				leftNode :
+					promoted ?
+							new PersistentHashMap.ArrayNode(null,newBitCount, newAnArray) :
+								new PersistentHashMap.BitmapIndexedNode(new AtomicReference<Thread>(), newBitmap, newBinArray);
+	}
 
 }
--- test/java/clojure/lang/BitmapIndexedNodeAndBitmapIndexedNodeSplicerTest.java
+++ test/java/clojure/lang/BitmapIndexedNodeAndBitmapIndexedNodeSplicerTest.java
@@ -6,62 +6,58 @@ import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
 
+import clojure.lang.PersistentHashMap.BitmapIndexedNode;
 import clojure.lang.PersistentHashMap.INode;
 
 public class BitmapIndexedNodeAndBitmapIndexedNodeSplicerTest implements SplicerTestInterface {
 
     final int shift = 0;
     final Splicer splicer = new BitmapIndexedNodeAndBitmapIndexedNodeSplicer();
-
-    public void test(Object leftKey, Object leftValue, Object rightKey, Object rightValue, boolean same) {
-
-        final INode leftNode = TestUtils.create(shift, leftKey, leftValue);
+        
+    public void test(Object leftKey0, Object leftValue0, Object leftKey1, Object leftValue1, int rightStart, int rightEnd, boolean same) {
+                
+        final INode leftNode = TestUtils.assoc(shift,
+                                               TestUtils.assoc(shift, BitmapIndexedNode.EMPTY, leftKey0, leftValue0, new Counts()),
+                                               leftKey1, leftValue1, new Counts());
 
         final Counts expectedCounts = new Counts();
-        final INode expected = TestUtils.assoc(shift, leftNode, rightKey, rightValue, expectedCounts);
-
-        final INode rightNode = TestUtils.create(shift, rightKey, rightValue);
-
+        final INode expectedNode = TestUtils.assocN(shift, leftNode, rightStart, rightEnd, expectedCounts);
+                
+        final INode rightNode = TestUtils.assocN(shift, BitmapIndexedNode.EMPTY, rightStart, rightEnd, new Counts());
+                
         final Counts actualCounts = new Counts(0, 0);
-        final INode actual = splicer.splice(shift, actualCounts, null, leftNode, null, rightNode);
+        final INode actualNode = splicer.splice(shift, actualCounts, null, leftNode, null, rightNode);
 
         assertEquals(expectedCounts, actualCounts);
-        assertNodeEquals(expected, actual);
-        if (same) assertSame(expected, actual); 
+        assertNodeEquals(expectedNode, actualNode);
+        if (same) assertSame(rightNode, actualNode); // is this right ?
     }
-
+        
     @Override
     @Test
     public void testDifferent() {
-        test(new HashCodeKey("leftKey", 2), "leftValue",
-             new HashCodeKey("rightKey", 4), "rightValue",
-             false);
+    	test(new HashCodeKey("key" + 1, 1), "value1", new HashCodeKey("key" + 2, 2), "value2", 3, 4, false);
+        test(new HashCodeKey("key" + 1, 1), "value1", new HashCodeKey("key" + 2, 2), "value2", 3, 18, false);
     }
 
     @Override
     @Test
     public void testSameKeyHashCode() {
-        final int hashCode = 3;
-        test(new HashCodeKey("leftKey", hashCode), "leftValue",
-             new HashCodeKey("rightKey", hashCode), "rightValue",
-             false);
+        test(new HashCodeKey("key" + 1, 3), "value1", new HashCodeKey("key" + 2, 4), "value2", 3, 4, false);
+        test(new HashCodeKey("key" + 1, 3), "value1", new HashCodeKey("key" + 2, 4), "value2", 3, 18, false);
     }
 
     @Override
     @Test
     public void testSameKey() {
-        final int hashCode = 3;
-        test(new HashCodeKey("key", hashCode), "leftValue",
-             new HashCodeKey("key", hashCode), "rightValue",
-             false);
+        test(new HashCodeKey("key" + 3, 3), "value1", new HashCodeKey("key" + 4, 4), "value2", 3, 4, false);
+        test(new HashCodeKey("key" + 3, 3), "value1", new HashCodeKey("key" + 4, 4), "value2", 3, 18, false);
     }
 
     @Override
     @Test
     public void testSameKeyAndValue() {
-        final int hashCode = 3;
-        test(new HashCodeKey("key", hashCode), "value",
-             new HashCodeKey("key", hashCode), "value",
-             false);
+        test(new HashCodeKey("key" + 3, 3), "value3", new HashCodeKey("key" + 4, 4), "value4", 3, 4, false); // TODO: FIXME
+        test(new HashCodeKey("key" + 3, 3), "value3", new HashCodeKey("key" + 4, 4), "value4", 3, 18, false); // TODO: FIXME
     }
 }
