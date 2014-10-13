ae9dcc5fc2d08805c398a0965f8c34ea4689dcca
Jules Gosnell <jules_gosnell@yahoo.com>
2014-10-13 18:48:49 +0100

    more promotion tests/fixes

--- test/java/clojure/lang/BitmapIndexedNodeAndKeyValuePairSplicerTest.java
+++ test/java/clojure/lang/BitmapIndexedNodeAndKeyValuePairSplicerTest.java
@@ -2,72 +2,60 @@ package clojure.lang;
 
 import static clojure.lang.TestUtils.assertNodeEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
 
 import clojure.lang.PersistentHashMap.INode;
+import clojure.lang.PersistentHashMap.BitmapIndexedNode;
 
 public class BitmapIndexedNodeAndKeyValuePairSplicerTest implements SplicerTestInterface {
     
-    final int shift = 0;
-    final int leftHashCode = 1;
-    final Object leftKey = new HashCodeKey("leftKey", leftHashCode);
-    final Object leftValue = "leftValue";
-    final INode leftNode = NodeUtils.create(shift, leftKey, leftValue);
-    
     final Splicer splicer = new BitmapIndexedNodeAndKeyValuePairSplicer();
-    
+    final int shift = 0;
+
+    public void test(int leftStart, int leftEnd, Object rightKey, Object rightValue, boolean same) {
+
+        final INode empty = BitmapIndexedNode.EMPTY;
+        final INode leftNode = TestUtils.assocN(shift, empty, leftStart, leftEnd, new Counts());
+
+        final Counts expectedCounts = new Counts();
+        final INode expectedNode = TestUtils.assoc(shift, leftNode, rightKey, rightValue, expectedCounts);
+
+        final Counts actualCounts = new Counts();
+        final INode actualNode = splicer.splice(shift, actualCounts, null, leftNode, rightKey, rightValue);
+
+        assertEquals(expectedCounts, actualCounts);
+        assertNodeEquals(expectedNode, actualNode);
+        if (same) assertSame(expectedNode, actualNode);
+    }
+
     @Override
     @Test
     public void testDifferent() {
-        
-        final int rightHashCode = 2;
-        final Object rightKey = new HashCodeKey("rightKey", rightHashCode);
-        final Object rightValue = "rightValue";
-
-        final INode expected = leftNode.assoc(shift, rightHashCode, rightKey, rightValue, new Box(null));
-
-        final Counts counts = new Counts(0, 0);
-        final INode actual = splicer.splice(shift, counts, null, leftNode, rightKey, rightValue);
-        
-        assertEquals(0, counts.sameKey);
-        assertNodeEquals(expected, actual);
+        test(2, 3, new HashCodeKey("key1", 1), "value1", false);
+        //        test(2, 17, new HashCodeKey("key1", 1), "value1", false);
     }
 
     @Override
     @Test
     public void testSameKeyHashCode() {
-        final Object rightKey = new HashCodeKey("rightKey", leftHashCode);
-        final Object rightValue = "rightValue";
-
-        final INode expected = leftNode.assoc(shift, leftHashCode, rightKey, rightValue, new Box(null));
-
-        final Counts counts = new Counts(0, 0);
-        final INode actual = splicer.splice(shift, counts, null, leftNode, rightKey, rightValue);
-        
-        assertEquals(0, counts.sameKey);
-        assertNodeEquals(expected, actual);
+        test(2, 3, new HashCodeKey("key1", 2), "value1", false);
+        test(2, 17, new HashCodeKey("key1", 2), "value1", false);
     }
 
     @Override
     @Test
     public void testSameKey() {
-        
-        final Object rightValue = "rightValue";
-
-        final INode expected = leftNode.assoc(shift, leftHashCode, leftKey, rightValue, new Box(null));
-
-        final Counts counts = new Counts(0, 0);
-        final INode actual = splicer.splice(shift, counts, null, leftNode, leftKey, rightValue);
-        
-        assertEquals(1, counts.sameKey);
-        assertNodeEquals(expected, actual);
+        test(2, 3, new HashCodeKey("key2", 2), "value1", false);
+        test(2, 17, new HashCodeKey("key2", 2), "value1", false);
     }
 
     @Override
     @Test
     public void testSameKeyAndValue() {
-        // TODO
+        test(2, 3, new HashCodeKey("key2", 2), "value2", false);
+        test(2, 17, new HashCodeKey("key2", 2), "value2", false);
     }
     
 }
