1af594a0a73ff3631afc66504bddb52d5942a19a
jules <jules_gosnell@yahoo.com>
2014-10-14 23:16:38 +0100

    make code more consistent

--- src/java/clojure/lang/ArrayNodeAndBitmapIndexedNodeSplicer.java
+++ src/java/clojure/lang/ArrayNodeAndBitmapIndexedNodeSplicer.java
@@ -13,45 +13,42 @@ class ArrayNodeAndBitmapIndexedNodeSplicer implements Splicer {
         final ArrayNode leftNode = (ArrayNode) leftValue;
         final BitmapIndexedNode rightNode = (BitmapIndexedNode) rightValue;
 
-        final INode[] newArray = new INode[32]; // allocate optimistically...
         final INode[] leftArray = leftNode.array;
         final Object[] rightArray = rightNode.array;
         final int rightBitmap = rightNode.bitmap;
 
-        int differences = 0;
-        int count = 0;
+        final INode[] newArray = new INode[32]; // allocate optimistically...
+
+        int differences = 0; // can we just return LHS
+        int empty = 0;
         for (int i = 0, j = 0; i < 32; i++) {
-            final INode lv = leftArray[i];
-            final boolean haveLeft = lv != null;
+            final INode leftSubNode = leftArray[i];
+            final boolean haveLeft = leftSubNode != null;
             final boolean haveRight = ((rightBitmap & (1 << i)) != 0);
 
             if (haveRight) {
-                count++;
-                final Object rk = rightArray[j++];
-                final Object rv = rightArray[j++];
+                final Object rightSubKey = rightArray[j++];
+                final Object rightSubValue = rightArray[j++];
                 if (haveLeft) {
                     // both sides present - splice them...
-                    if (lv !=
-                        (newArray[i]
-                         = NodeUtils.splice(shift + 5, counts, null, lv, rk, rv)))
-                        differences++;
+                    final INode newSubNode = NodeUtils.splice(shift + 5, counts, null, leftSubNode, rightSubKey, rightSubValue);
+                    newArray[i] = newSubNode;
+                    if (leftSubNode != newSubNode) differences++;
                 } else {
                     // only rhs present
-                    newArray[i] = (rk == null) ?
-                        (INode) rv :
-                        NodeUtils.create(shift + 5, rk, rv);
+                    newArray[i] = (rightSubKey == null) ? (INode) rightSubValue : NodeUtils.create(shift + 5, rightSubKey, rightSubValue);
                     differences++;
                 }
             } else { // not haveRight
                 if (haveLeft) {
-                    count++;
-                    newArray[i] = lv;
+                    newArray[i] = leftSubNode;
                 } else {
                     // neither lhs or rhs present...
+                	empty++;
                 }
             }
         }
 
-        return differences > 0 ?  new ArrayNode(null, count, newArray) : leftNode;
+        return differences == 0 ? leftNode : new ArrayNode(null, 32 - empty, newArray);
     }
 }
