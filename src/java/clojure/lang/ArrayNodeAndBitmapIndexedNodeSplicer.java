417aa74b63138657f68cb869e92fa9f21ae00305
Jules Gosnell <jules_gosnell@yahoo.com>
2014-10-15 19:21:42 +0100

    more consistent code

--- src/java/clojure/lang/ArrayNodeAndBitmapIndexedNodeSplicer.java
+++ src/java/clojure/lang/ArrayNodeAndBitmapIndexedNodeSplicer.java
@@ -19,36 +19,37 @@ class ArrayNodeAndBitmapIndexedNodeSplicer implements Splicer {
 
         final INode[] newArray = new INode[32]; // allocate optimistically...
 
-        int differences = 0; // can we just return LHS
+        int leftDifferences = 0; // can we just return LHS
         int empty = 0;
-        for (int i = 0, j = 0; i < 32; i++) {
+        int rightIndex = 0;
+        for (int i = 0; i < 32; i++) {
             final INode leftSubNode = leftArray[i];
             final boolean haveLeft = leftSubNode != null;
             final boolean haveRight = ((rightBitmap & (1 << i)) != 0);
 
             if (haveRight) {
-                final Object rightSubKey = rightArray[j++];
-                final Object rightSubValue = rightArray[j++];
+                final Object rightSubKey = rightArray[rightIndex++];
+                final Object rightSubValue = rightArray[rightIndex++];
                 if (haveLeft) {
                     // both sides present - splice them...
                     final INode newSubNode = NodeUtils.splice(shift + 5, counts, null, leftSubNode, rightSubKey, rightSubValue);
                     newArray[i] = newSubNode;
-                    if (leftSubNode != newSubNode) differences++;
+                    if (leftSubNode != newSubNode) leftDifferences++;
                 } else {
                     // only rhs present
                     newArray[i] = (rightSubKey == null) ? (INode) rightSubValue : NodeUtils.create(shift + 5, rightSubKey, rightSubValue);
-                    differences++;
+                    leftDifferences++;
                 }
             } else { // not haveRight
                 if (haveLeft) {
                     newArray[i] = leftSubNode;
                 } else {
                     // neither lhs or rhs present...
-                	empty++;
+                    empty++;
                 }
             }
         }
 
-        return differences == 0 ? leftNode : new ArrayNode(null, 32 - empty, newArray);
+        return leftDifferences == 0 ? leftNode : new ArrayNode(null, 32 - empty, newArray);
     }
 }
