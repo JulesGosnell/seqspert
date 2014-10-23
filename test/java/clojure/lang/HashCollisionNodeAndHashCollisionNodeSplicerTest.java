d5700d0960d49eb22aabb958a26ef0cc985282c9
Jules Gosnell <jules_gosnell@yahoo.com>
2014-10-23 19:12:11 +0100

    rationalise test code

--- test/java/clojure/lang/HashCollisionNodeAndHashCollisionNodeSplicerTest.java
+++ test/java/clojure/lang/HashCollisionNodeAndHashCollisionNodeSplicerTest.java
@@ -14,7 +14,37 @@ import clojure.lang.PersistentHashMap.INode;
 
 public class HashCollisionNodeAndHashCollisionNodeSplicerTest implements SplicerTestInterface {
 
-    // TODO: refactor
+    final int shift = 0;
+
+    public void test(Object key0, Object value0, Object key1, Object value1,
+                     Object key2, Object value2, Object key3, Object value3, Object key4, Object value4,
+                     boolean sameLeft, boolean sameRight) {
+
+        final INode leftNode = TestUtils.create(shift, key0, value0, key1, value1);
+        final INode rightNode =  TestUtils.create(shift, key2, value2, key3, value3, key4, value4);
+
+        final Counts expectedCounts = new Counts();
+        final INode expectedNode =
+            TestUtils.assoc(shift, leftNode, key2, value2, key3, value3, key4, value4, expectedCounts);
+
+        final Counts actualCounts = new Counts();
+        final INode actualNode = NodeUtils.splice(shift, actualCounts, null, leftNode, null, rightNode);
+
+        assertEquals(expectedCounts, actualCounts);
+        assertNodeEquals(expectedNode, actualNode);
+        if (sameLeft) TestUtils.assertSame(leftNode, expectedNode, actualNode);
+        if (sameRight) assertSame(rightNode, actualNode);
+    }
+
+    final int hashCode = 2;
+    final Object key0 = new HashCodeKey("key0", hashCode);
+    final Object key1 = new HashCodeKey("key1", hashCode);
+    final Object key2 = new HashCodeKey("key2", hashCode);
+    final Object key3 = new HashCodeKey("key3", hashCode);
+    final Object value0 = "value0";
+    final Object value1 = "value1";
+    final Object value2 = "value2";
+    final Object value3 = "value3";
 
     @Override
     @Test
@@ -30,60 +60,8 @@ public class HashCollisionNodeAndHashCollisionNodeSplicerTest implements Splicer
         final Object value1 = "value1";
         final Object value2 = "value2";
         final Object value3 = "value3";
-        final HashCollisionNode leftNode   = new HashCollisionNode(null, leftHashCode,  2, new Object[]{key0, value0, key1, value1});
-        final HashCollisionNode rightNode =  new HashCollisionNode(null, rightHashCode, 2, new Object[]{key2, value2, key3, value3});
 
-        final AtomicReference<Thread> edit = new AtomicReference<Thread>();
-        INode expected = BitmapIndexedNode.EMPTY;
-        Box addedLeaf = null;
-        int expectedCounts = 0;
-        addedLeaf = new Box(null);
-        expected = expected.assoc(edit, shift, key0.hashCode(), key0, value0, addedLeaf);
-        expectedCounts += (addedLeaf.val == addedLeaf) ? 0 : 1;
-        addedLeaf = new Box(null);      
-        expected = expected.assoc(edit, shift, key1.hashCode(), key1, value1, addedLeaf);
-        expectedCounts += (addedLeaf.val == addedLeaf) ? 0 : 1;
-        addedLeaf = new Box(null);
-        expected = expected.assoc(edit, shift, key2.hashCode(), key2, value2, addedLeaf);
-        expectedCounts += (addedLeaf.val == addedLeaf) ? 0 : 1;
-        addedLeaf = new Box(null);      
-        expected = expected.assoc(edit, shift, key3.hashCode(), key3, value3, addedLeaf);
-        expectedCounts += (addedLeaf.val == addedLeaf) ? 0 : 1;
-
-        final Counts counts = new Counts(NodeUtils.resolveRight, 0, 0); // TODO: what about resolveLeft ?
-        final INode actual = NodeUtils.splice(shift, counts, null, leftNode, null, rightNode);
-        assertNodeEquals(expected, actual);
-        assertEquals(expectedCounts, counts.sameKey);
-    }
-
-    final int shift = 0;
-    final int hashCode = 2;
-    final Object key0 = new HashCodeKey("key0", hashCode);
-    final Object key1 = new HashCodeKey("key1", hashCode);
-    final Object key2 = new HashCodeKey("key2", hashCode);
-    final Object key3 = new HashCodeKey("key3", hashCode);
-    final Object value0 = "value0";
-    final Object value1 = "value1";
-    final Object value2 = "value2";
-    final Object value3 = "value3";
-
-    public void test(Object key0, Object value0, Object key1, Object value1,
-                     Object key2, Object value2, Object key3, Object value3, Object key4, Object value4,
-                     boolean sameLeft, boolean sameRight) {
-
-        final INode leftNode = TestUtils.create(shift, key0, value0, key1, value1);
-        final INode rightNode =  TestUtils.create(shift, key2, value2, key3, value3, key4, value4);
-
-        final Counts expectedCounts = new Counts();
-        final INode expectedNode = TestUtils.assoc(shift, leftNode, key2, value2, key3, value3, key4, value4, expectedCounts);
-
-        final Counts actualCounts = new Counts();
-        final INode actualNode = NodeUtils.splice(shift, actualCounts, null, leftNode, null, rightNode);
-
-        assertEquals(expectedCounts, actualCounts);
-        assertNodeEquals(expectedNode, actualNode);
-        if (sameLeft) TestUtils.assertSame(leftNode, expectedNode, actualNode);
-        if (sameRight) assertSame(rightNode, actualNode);
+        test(key0, value0, key1, value1, key2, value2, key3, value3, null, null, false, false);
     }
 
     @Override
