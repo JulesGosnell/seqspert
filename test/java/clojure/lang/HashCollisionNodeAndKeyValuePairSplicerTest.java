a38ed743214758deb075c014b387e87c34a56a2a
Jules Gosnell <jules_gosnell@yahoo.com>
2014-10-23 19:30:19 +0100

    rationalise test code

--- test/java/clojure/lang/HashCollisionNodeAndKeyValuePairSplicerTest.java
+++ test/java/clojure/lang/HashCollisionNodeAndKeyValuePairSplicerTest.java
@@ -10,31 +10,34 @@ import clojure.lang.PersistentHashMap.HashCollisionNode;
 import clojure.lang.PersistentHashMap.INode;
 
 public class HashCollisionNodeAndKeyValuePairSplicerTest implements SplicerTestInterface {
-
+    
+    
+    final Splicer splicer = new HashCollisionNodeAndKeyValuePairSplicer();
     final int shift = 0;
+
+    public void test(int rightHashCode, Object rightKey, Object rightValue, boolean same) {
+
+        final INode leftNode = new HashCollisionNode(null, hashCode, 2, new Object[]{key0, value0, key1, value1});
+
+        final Counts expectedCounts = new Counts(NodeUtils.resolveRight, 0, 0);
+        final INode expectedNode = 
+            TestUtils.assoc(shift, leftNode, rightKey, rightValue, expectedCounts);
+
+        final Counts actualCounts = new Counts(NodeUtils.resolveRight, 0, 0); // TODO:  resolveLeft ?
+        final INode actualNode = splicer.splice(shift, actualCounts, null, leftNode, rightKey, rightValue);
+
+        assertEquals(expectedCounts, actualCounts);
+        assertNodeEquals(expectedNode, actualNode);
+        if (same) assertSame(expectedNode, actualNode);
+    }
+
+    // TODO: inline and tidy up
+
     final int hashCode = 2;
     final Object key0 = new HashCodeKey("key0", hashCode);
     final Object key1 = new HashCodeKey("key1", hashCode);
     final Object value0 = "value0";
     final Object value1 = "value1";
-
-    // TODO: refactor
-    public void test(int rightHashCode, Object rightKey, Object rightValue, boolean same) {
-        final INode leftNode = new HashCollisionNode(null, hashCode, 2, new Object[]{key0, value0, key1, value1});
-
-        final Box addedLeaf = new Box(null);
-        final INode expectedNode = leftNode.assoc(shift, rightHashCode , rightKey, rightValue, addedLeaf);
-        final int expectedCounts = (addedLeaf.val == addedLeaf) ? 0 : 1;
-        final Splicer splicer = new HashCollisionNodeAndKeyValuePairSplicer();
-        final Counts actualCounts = new Counts(NodeUtils.resolveRight, 0, 0); // TODO: what about resolveLeft ?
-        final INode actualNode =  splicer.splice(shift, actualCounts, null, leftNode, rightKey, rightValue);
-
-        assertEquals(expectedCounts, actualCounts.sameKey);
-        if (same) 
-            assertSame(expectedNode, actualNode);
-        else
-            assertNodeEquals(expectedNode, actualNode);
-    }
 
     @Test
     @Override
