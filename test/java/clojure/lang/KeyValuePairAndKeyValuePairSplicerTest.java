c3066428c6bd1e8ecaa545199b947388fa4241e1
jules <jules_gosnell@yahoo.com>
2014-10-25 14:02:08 +0100

    more test coverage

--- test/java/clojure/lang/KeyValuePairAndKeyValuePairSplicerTest.java
+++ test/java/clojure/lang/KeyValuePairAndKeyValuePairSplicerTest.java
@@ -2,7 +2,7 @@ package clojure.lang;
 
 import static clojure.lang.TestUtils.assertNodeEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -16,8 +16,7 @@ public class KeyValuePairAndKeyValuePairSplicerTest implements SplicerTestInterf
     final int shift = 0;
 
     public void test(Object leftKey, Object leftValue,
-                     Object rightKey, Object rightValue,
-                     boolean same) {
+                     Object rightKey, Object rightValue, boolean same) {
 
         final Counts expectedCounts = new Counts();
         final INode expectedNode = 
@@ -31,36 +30,37 @@ public class KeyValuePairAndKeyValuePairSplicerTest implements SplicerTestInterf
             splicer.splice(shift, actualCounts, leftKey, leftValue, rightKey, rightValue);
         
         assertEquals(expectedCounts, actualCounts);
-        assertNodeEquals(expectedNode, actualNode);
-        if (same) assertSame(expectedNode, actualNode);
+        if (same) {
+        	assertNull(actualNode);
+        } else {
+            assertNodeEquals(expectedNode, actualNode);
+        }
     }
     
     @Test
     @Override
     public void testDifferent() {
         test(new HashCodeKey("key1", 1), "value1", new HashCodeKey("key2", 2), "value2", false);
+        test(new HashCodeKey("key1", (1 << 5) | 1), "value1", new HashCodeKey("key2", 1), "value2", false);
     }
 
-    @Ignore
+    @Ignore // TODO: this returns an HCN - expected is a BIN containing an HCN - needs some thought...
     @Test
     @Override
     public void testSameKeyHashCode() {
         test(new HashCodeKey("key1", 1), "value1", new HashCodeKey("key2", 1), "value2", false);
     }
 
-    @Ignore
     @Test
     @Override
     public void testSameKey() {
-        // TODO: returns null...
-        test(new HashCodeKey("key1", 1), "value1", new HashCodeKey("key1", 1), "value2", false);
+        test(new HashCodeKey("key1", 1), "value1", new HashCodeKey("key1", 1), "value2", true);
     }
 
     @Test
     @Override
     public void testSameKeyAndValue() {
-        // TODO: not sure that this makes sense...
-        //test(new HashCodeKey("key1", 1), "value1", new HashCodeKey("key1", 1), "value1", true);
+    	test(new HashCodeKey("key1", 1), "value1", new HashCodeKey("key1", 1), "value1", true);
     }
 
 }
